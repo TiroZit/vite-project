b1f7275f4d4c069a4a9c5d3420e3a49eec3e7506
Nikita Avdeev <nikita.avdeev@xpage.ru>
2023-11-01 17:38:38 +0500

    yarn

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import VitePluginSvgSpritemap from '@spiriit/vite-plugin-svg-spritemap'
+import VitePluginSvgSpritemap from '@spiriit/vite-plugin-svg-spritemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
+	plugins: [
 		vue(),
-		VitePluginSvgSpritemap('./src/assets/*.svg',{
-			styles: {
-				lang: 'scss',
-				filename: './src/scss/spritemap.scss'
+		VitePluginSvgSpritemap('./src/assets/**/*.svg', {
+			prefix: 'icon-',
+			injectSVGOnDev: true,
+			output: {
+				filename: '../img/icons.svg',
 			},
-		})
+			styles: './src/scss/sprite.scss',
+		}),
 	],
 })
